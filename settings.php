8def725e372a55c080191d13a6c3e8f6b04f0d80
jmuszka <josh@jmuszka.com>
2023-12-28 02:06:09 -0500

    Layout for settings menu

--- settings.php
+++ settings.php
@@ -7,6 +7,17 @@
         <link rel="stylesheet" href="css/settings.css"/>
     </head>
 
+    <?php
+        include 'php/format_data.php';
+
+        //Get session info
+        session_start();
+        $USERNAME = $_SESSION['userInfo']->username;
+        $NAME = $_SESSION['userInfo']->name;
+        $ACCOUNT_CREATED = $_SESSION['userInfo']->accountCreated;
+        if (!$USERNAME || !$NAME || !$ACCOUNT_CREATED) header("Location: login.php"); //Return to login if no info provided
+    ?>
+
     <body>
 
         <!-- Settings button -->
@@ -16,26 +27,26 @@
 
         <!-- Greeting panel -->
         <div id="greetingPanel">
+
             <h1 class="heading">Settings</h1>
-            <h3 class="subheading">Account information</h3>
-            <p>NAME</p>
-            <p>created</p>
-            <p>eadad</p>
-            <p>Current storage</p>
+            <br>
+            <hr>
+
+            <h3 class="subheading">Account information:</h3><br>
+            <p>Name: <?php echo "<b>$NAME</b>"; ?></p>
+            <p>Username: <?php echo "<b>$USERNAME</b>"; ?></p>
+            <p>Account created: <?php echo "<b>" .  epochToGregorian($ACCOUNT_CREATED) . "</b>"; ?></p>
+            <p>Max upload size: <b>10 MB</b></p>
+            <p>Storage: <b>0.5 GB out of 10 GB used</b></p>
             <br>
             <hr/>
 
-            <h3 class="subheading">Preferences</h3>
-            <p>Theme</p>
-            <p>Font size</p>
-            <p>awdaw</p>
-            <br>
-            <hr/>
-
-            <h3 class="subheading">Admin dashboard</h3>
-            <p>Create new user</p>
-            <p>Manage users</p>
-            <p>Change defaults</p>
+            <h3 class="subheading">Preferences</h3><br>
+            <p>Theme: <b>Dark</b></p>
+            <p>Font size: <b>Medium</b></p>
+            <p>Accent color: <b>#09b598 <span style="color: #09b598; display: inline;">■</span></b></p>
+            <p>Background (photo panel): <b>fuji.webp</b></p>
+            <p>Background (login page): <b>schwarzwald.jpg</b></p>
 
         </div>
 
